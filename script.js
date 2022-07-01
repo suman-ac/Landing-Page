ee3b33fbd8ba5e40113d893a63d75056bd12d7e7
suman-ac <108453905+suman-ac@users.noreply.github.com>
2022-07-01 11:48:05 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function hideFunction() {
+    document.getElementById("hideme").style.display = "none";
+    document.getElementById("oldbutton").style.display = "none";
+    document.getElementById("newbutton").style.display = "block";
+
+}
+
+function showFunction() {
+    document.getElementById("hideme").style.display = "block";
+    document.getElementById("oldbutton").style.display = "block";
+    document.getElementById("newbutton").style.display = "none";
+
+}
